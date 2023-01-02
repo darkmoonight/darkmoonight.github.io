9ab6d275eaebd811163875182d0f35498e6affc9
Yoshi <dleonavichus@mail.ru>
2023-01-02 22:15:52 +0300

    fix navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,10 @@ const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
     const scrollY = window.pageYOffset
-
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 50,
             sectionId = current.getAttribute('id')
-
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
         } else {
