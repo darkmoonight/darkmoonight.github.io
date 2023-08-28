2150503aeee3f043851618f5c4c94d2e51344707
Yoshi <dleonavichus@mail.ru>
2023-08-28 17:01:41 +0300

    delete app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,115 +1,136 @@
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-    const scrollY = window.pageYOffset
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 50,
-            sectionId = current.getAttribute('id')
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else {
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive)
-
+window.addEventListener("scroll", scrollActive);
 
 function scrollHeader() {
-    const header = document.getElementById('header')
-    if (this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  const header = document.getElementById("header");
+  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById("scroll-up");
 
-    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 if (selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
 
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
-const contactForm = document.getElementById('contact-form'),
-    contactName = document.getElementById('contact-name'),
-    contactSubject = document.getElementById('contact-subject'),
-    contactEmail = document.getElementById('contact-email'),
-    contactMessenge = document.getElementById('contact-messenge'),
-    contactMessage = document.getElementById('contact-message')
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactSubject = document.getElementById("contact-subject"),
+  contactEmail = document.getElementById("contact-email"),
+  contactMessenge = document.getElementById("contact-messenge"),
+  contactMessage = document.getElementById("contact-message");
 
 const sendEmail = (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    if (contactName.value === '' || contactSubject.value === '' || contactEmail.value === '' || contactMessenge.value === '') {
-        contactMessage.classList.remove('color-green')
-        contactMessage.classList.add('color-red')
+  if (
+    contactName.value === "" ||
+    contactSubject.value === "" ||
+    contactEmail.value === "" ||
+    contactMessenge.value === ""
+  ) {
+    contactMessage.classList.remove("color-green");
+    contactMessage.classList.add("color-red");
 
-        contactMessage.textContent = 'Запишите все входные поля'
-    } else {
-        emailjs.sendForm('service_afpnn2t', 'template_49g41lj', '#contact-form', 'KcAEd2ZIMT9neZKjk')
-            .then(() => {
-                contactMessage.classList.add('color-green')
-                contactMessage.textContent = 'Сообщение отправлено'
+    contactMessage.textContent = "Запишите все входные поля";
+  } else {
+    emailjs
+      .sendForm(
+        "service_afpnn2t",
+        "template_49g41lj",
+        "#contact-form",
+        "KcAEd2ZIMT9neZKjk"
+      )
+      .then(
+        () => {
+          contactMessage.classList.add("color-green");
+          contactMessage.textContent = "Сообщение отправлено";
 
-                setTimeout(() => {
-                    contactMessage.textContent = ''
-                }, 5000)
-            }, (error) => {
-                alert('УПС! ЧТО-ТО ПОШЛО НЕ ТАК...', error)
-            })
-        contactName.value = ''
-        contactSubject.value = ''
-        contactEmail.value = ''
-        contactMessenge.value = ''
-    }
-}
-contactForm.addEventListener('submit', sendEmail)
-
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("УПС! ЧТО-ТО ПОШЛО НЕ ТАК...", error);
+        }
+      );
+    contactName.value = "";
+    contactSubject.value = "";
+    contactEmail.value = "";
+    contactMessenge.value = "";
+  }
+};
+contactForm.addEventListener("submit", sendEmail);
 
 const filterButtons = document.querySelector("#filter-btns").children;
 const items = document.querySelector(".project_contetnt").children;
 
 for (let i = 0; i < filterButtons.length; i++) {
-    filterButtons[i].addEventListener("click", function () {
-        for (let j = 0; j < filterButtons.length; j++) {
-            filterButtons[j].classList.remove("project_filter-active")
-        }
-        this.classList.add("project_filter-active");
-        const target = this.getAttribute("data-target")
+  filterButtons[i].addEventListener("click", function () {
+    for (let j = 0; j < filterButtons.length; j++) {
+      filterButtons[j].classList.remove("project_filter-active");
+    }
+    this.classList.add("project_filter-active");
+    const target = this.getAttribute("data-target");
 
-        for (let k = 0; k < items.length; k++) {
-            items[k].style.display = "none";
-            if (target == items[k].getAttribute("data-id")) {
-                items[k].style.display = "block";
-            }
-            if (target == "All") {
-                items[k].style.display = "block";
-            }
-        }
-
-    })
+    for (let k = 0; k < items.length; k++) {
+      items[k].style.display = "none";
+      if (target == items[k].getAttribute("data-id")) {
+        items[k].style.display = "block";
+      }
+      if (target == "All") {
+        items[k].style.display = "block";
+      }
+    }
+  });
 }
